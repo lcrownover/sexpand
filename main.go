877d32fbe967f7541ba28cfc9750484352b973fc
Lucas Crownover <lcrownover127@gmail.com>
2024-06-22 20:29:56 +0100

    more work

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -43,11 +44,89 @@ func splitOutsideRange(s string) []string {
 	return outs
 }
 
-// expandRange expands a single SLURM range notation into a list of strings
-func expandRange(s string) []string {
-	prefix := strings.Split(s, "[")[0]
-	fmt.Printf("prefix:		%s\n", prefix)
-	return nil
+// splitPrefix takes a SLURM group range notation and returns the prefix and the notation separately
+// this function expects that there is a range notation in the group and will panic if not
+func splitPrefix(group string) (string, string) {
+	if !strings.Contains(group, "[") {
+		panic("splitPrefix group must contain range notation. check before using.")
+	}
+	prefix := ""
+	groupRange := ""
+	beforePrefix := true
+	for _, c := range group {
+		if c == '[' {
+			beforePrefix = false
+		}
+		if beforePrefix {
+			prefix = prefix + string(c)
+		} else {
+			groupRange = groupRange + string(c)
+		}
+	}
+	return prefix, groupRange
+}
+
+// expandRange expands a single bracketed range notation into a
+// list of strings
+func expandRange(r string) ([]string, error) {
+	s := strings.Split(r, "-")
+	groupStart := s[0]
+	groupEnd := s[1]
+	rangeLength := len(groupStart)
+	// now convert start and end to integers
+	startNum, err := strconv.Atoi(groupStart)
+	if err != nil {
+		return nil, fmt.Errorf("invalid number used in range: %d", startNum)
+	}
+	endNum, err := strconv.Atoi(groupEnd)
+	if err != nil {
+		return nil, fmt.Errorf("invalid number used in range: %d", endNum)
+	}
+	// validate that the start num comes before the end num for a valid range
+	if startNum >= endNum {
+		return nil, fmt.Errorf("range start must be smaller than range end: [%d-%d]", startNum, endNum)
+	}
+	// generate the list of integers
+	nums := []int{}
+	for i := startNum; i <= endNum; i++ {
+		nums = append(nums, i)
+	}
+	// reformat that list to strings of the correct padded length
+	// from the original length
+	numStrs := []string{}
+	for _, n := range nums {
+		paddedNum := fmt.Sprintf("%0*d", rangeLength, n)
+		numStrs = append(numStrs, paddedNum)
+	}
+	return numStrs, nil
+}
+
+// expandGroup expands a single prefix and SLURM range notation
+// into a list of strings
+func expandGroup(prefix string, group string) ([]string, error) {
+	// first we find the length of runes for the start of the range
+	group = strings.ReplaceAll(group, "[", "")
+	group = strings.ReplaceAll(group, "]", "")
+	expandedParts := []string{}
+	csParts := strings.Split(group, ",")
+	for _, p := range csParts {
+		if strings.Contains(p, "-") {
+			expandedRange, err := expandRange(p)
+			if err != nil {
+				return nil, fmt.Errorf("invalid range: %s", p)
+			}
+			expandedParts = append(expandedParts, expandedRange...)
+		} else {
+			// simple part, no range notation "-"
+			expandedParts = append(expandedParts, p)
+		}
+	}
+	// add the prefix to all of them
+	outs := []string{}
+	for _, n := range expandedParts {
+		outs = append(outs, fmt.Sprintf("%s%s", prefix, n))
+	}
+	return outs, nil
 }
 
 func SExpand(s string) ([]string, error) {
--- main_test.go
+++ main_test.go
@@ -29,23 +29,76 @@ func TestSplitOutsideRange(t *testing.T) {
 	}
 }
 
-func TestExpandRange(t *testing.T) {
+func TestSplitPrefix(t *testing.T) {
 	inputs := []string{
 		"n[01-02]",
-		"n[0-2]",
-		"n[05-07,09]",
-		"[05-07,09]",
+		"[0-2]",
+		"np[05-07,09]",
+	}
+	expected := [][]string{
+		{"n", "[01-02]"},
+		{"", "[0-2]"},
+		{"np", "[05-07,09]"},
+	}
+
+	for i := range inputs {
+		testInput := inputs[i]
+		want := expected[i]
+		got0, got1 := splitPrefix(testInput)
+		if got0 != want[0] || got1 != want[1] {
+			t.Errorf("got (%s, %s), want (%s, %s)", got0, got1, want[0], want[1])
+		}
+	}
+}
+
+func TestExpandRange(t *testing.T) {
+	inputs := []string{
+		"01-02",
+		"0-2",
+		"05-07",
+		"05-07",
+	}
+	expected := [][]string{
+		{"01", "02"},
+		{"0", "1", "2"},
+		{"05", "06", "07"},
+		{"05", "06", "07"},
+	}
+
+	for i := range inputs {
+		testInput := inputs[i]
+		want := expected[i]
+		got, err := expandRange(testInput)
+		if err != nil {
+			t.Error(err)
+		}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	}
+}
+
+func TestExpandGroup(t *testing.T) {
+	inputs := [][]string{
+		{"n", "[01-02]"},
+		{"n", "[0-2]"},
+		{"np", "[05-07,09,11,23-25]"},
+		{"", "[05-07,09]"},
 	}
 	expected := [][]string{
 		{"n01", "n02"},
 		{"n0", "n1", "n2"},
+		{"np05", "np06", "np07", "np09", "np11", "np23", "np24", "np25"},
 		{"05", "06", "07", "09"},
 	}
 
 	for i := range inputs {
 		testInput := inputs[i]
 		want := expected[i]
-		got := expandRange(testInput)
+		got, err := expandGroup(testInput[0], testInput[1])
+		if err != nil {
+			t.Error(err)
+		}
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %s, want %s", got, want)
 		}
